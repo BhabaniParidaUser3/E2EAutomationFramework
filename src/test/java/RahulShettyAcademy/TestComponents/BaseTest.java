9875e4da0f6ef53b4953a8f3d889a9706964b9f0
BhabaniParidaUser3 <bhabaniparida1995@gmail.com>
2024-02-26 01:39:43 +0530

    3rd commit

--- src/test/java/RahulShettyAcademy/TestComponents/BaseTest.java
+++ src/test/java/RahulShettyAcademy/TestComponents/BaseTest.java
@@ -1,5 +1,24 @@
 package RahulShettyAcademy.TestComponents;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
+	
+	public void initializationDriver()
+	{
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions option = new ChromeOptions();
+        option.addArguments("--remote-allow-origins=*");
+		WebDriver driver=new ChromeDriver(option);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		
+	}
 
 }
